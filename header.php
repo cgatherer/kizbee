d6dfc21b9f64111f6382f971834f98242259d95a
Christopher Gatherer <box@ChristophersMBP.home>
2017-09-23 17:30:07 -0400

    Favicon

--- header.php
+++ header.php
@@ -12,6 +12,13 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/img/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/img/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/img/favicon-16x16.png">
+<link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/img/manifest.json">
+<link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/img/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="theme-color" content="#ffffff">
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
