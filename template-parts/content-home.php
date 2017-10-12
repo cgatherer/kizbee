51dd134d5ad5c84b00a37826bb341a841f9b6366
Christopher Gatherer <box@Christophers-MacBook-Pro.local>
2017-10-12 18:27:25 -0400

    products

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -24,29 +24,19 @@
 			<?php
 			  	$taxonomy     = 'product_cat';
 			  	$orderby      = 'name';  
-			  	$show_count   = 0;      // 1 for yes, 0 for no
-			  	$pad_counts   = 0;      // 1 for yes, 0 for no
-			  	$hierarchical = 1;      // 1 for yes, 0 for no  
-			  	$title        = '';  
-			  	$empty        = 0;
 
 			  	$args = array(
 			         'taxonomy'     => $taxonomy,
-			         'orderby'      => $orderby,
-			         'show_count'   => $show_count,
-			         'pad_counts'   => $pad_counts,
-			         'hierarchical' => $hierarchical,
-			         'title_li'     => $title,
-			         'hide_empty'   => $empty
+			         'orderby'      => $orderby
 				);
 			 
 				$all_categories = get_categories($args);
 
 				foreach ($all_categories as $cat) {
-			    	if($cat->category_parent == 0) {
+			    	// if($cat->category_parent == 0) {
 			        	$category_id = $cat->term_id;       
 			        	echo '<a href="'. get_term_link($cat->slug, 'product_cat') .'">'. $cat->name .'</a>';
-			    	}       
+			    	// }       
 				}
 			?>
 		</div>
