3864da92fbd746878dfbf439ecf79e128e6474e7
Christopher Gatherer <box@ChristophersMBP.home>
2017-10-12 21:07:17 -0400

    products image

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -25,9 +25,6 @@ $wrapper_classes   = apply_filters( 'woocommerce_single_product_image_gallery_cl
 
 	<div class="container">
 		<div class="span12 margin-top">
-
-			<?php echo $post_thumbnail_id;?>
-
 			<?php
 			  	$taxonomy     = 'product_cat';
 			  	$orderby      = 'name';  
@@ -74,6 +71,11 @@ $wrapper_classes   = apply_filters( 'woocommerce_single_product_image_gallery_cl
 					$html  = '<div data-thumb="' . get_the_post_thumbnail_url( $post->ID, 'shop_thumbnail' ) . '" class="woocommerce-product-gallery__image"><a href="' . esc_url( $full_size_image[0] ) . '">';
 					$html .= get_the_post_thumbnail( $post->ID, 'shop_single', $attributes );
 					$html .= '</a></div>';
+				} else {
+					$html  = '<div class="woocommerce-product-gallery__image--placeholder">';
+					$html .= sprintf( '<img src="%s" alt="%s" class="wp-post-image" />', esc_url( wc_placeholder_img_src() ), esc_html__( 'Awaiting product image', 'woocommerce' ) );
+					$html .= '</div>';
+				}
 
 				echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', $html, get_post_thumbnail_id( $post->ID ) );
 
