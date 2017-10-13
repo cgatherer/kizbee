525cdc04da7953ab16db0d061fb92b15091b0d4d
Christopher Gatherer <box@ChristophersMBP.home>
2017-10-13 00:52:11 -0400

    products image

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -14,23 +14,7 @@ global $product;
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<div class="container">
-		
-		<?php
-			$args = array( 'post_type' => 'product', 'posts_per_page' => 80, 'product_cat' => 'profiler', 'orderby' => 'rand' );
-
-			$loop = new WP_Query( $args );
-			while ( $loop->have_posts() ) : $loop->the_post(); global $product; ?>
-			    <div class="dvThumb col-xs-4 col-sm-3 col-md-3 profiler-select profiler<?php echo the_title(); ?>" data-profile="<?php echo $loop->post->ID; ?>">
-			        <?php $featured_image = wp_get_attachment_image_src( get_post_thumbnail_id($loop->post->ID)); ?>
-			        <?php if($featured_image) { ?>
-			        <img src="<?php $featured_image[0]; ?>" data-id="<?php echo $loop->post->ID; ?>">
-			        <?php } ?>
-			        <p><?php the_title(); ?></p>
-			        <span class="price"><?php echo $product->get_price_html(); ?></span>
-			    </div>
-			<?php endwhile; ?>
-
-		<!-- <div class="span12 margin-top">
+		<div class="span12 margin-top">
 			<?php
 			  	$taxonomy     = 'product_cat';
 			  	$orderby      = 'name';  
@@ -59,9 +43,9 @@ global $product;
 			    	}       
 				}
 			?>
-		</div> -->
+		</div>
 
-		<!-- <div class="span12 group margin-bottom">
+		<div class="span12 group margin-bottom">
 
 		<?php
 			$params = array(
@@ -80,6 +64,7 @@ global $product;
 						
 						<div class="span6 tiles" style="background: url('<?php the_field( 'new_product_image' ); ?>'); background-size: cover;">
 							<h2><?php the_title();?></h2>
+
 						</div>
 					
 					<?php endwhile;
@@ -91,8 +76,7 @@ global $product;
 					</p>
 		<?php endif; ?>
 
-		</div> -->
-
+		</div>
 	</div>
 
 </div>
