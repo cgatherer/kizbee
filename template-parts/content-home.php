633dce6ef2f3be5a7f121f884da43bbbd2872e69
Christopher Gatherer <box@ChristophersMBP.home>
2017-10-12 23:12:20 -0400

    products image

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -64,7 +64,7 @@ global $product;
 						
 						<div class="span6 tiles" style="background: url('<?php the_field( 'new_product_image' ); ?>'); background-size: cover;">
 							<h2><?php the_title();?></h2>
-							<p><?php echo $product->get_price_html(); ?></p>
+
 						</div>
 					
 					<?php endwhile;
@@ -75,21 +75,6 @@ global $product;
 					     <?php _e( 'No Products' ); // (6) ?>
 					</p>
 		<?php endif; ?>
-
-		<!-- <?php
-			$args = array( 'post_type' => 'product', 'posts_per_page' => 80, 'product_cat' => 'profiler', 'orderby' => 'rand' );
-
-			$loop = new WP_Query( $args );
-			while ( $loop->have_posts() ) : $loop->the_post(); global $product; ?>
-			    <div class="dvThumb col-xs-4 col-sm-3 col-md-3 profiler-select profiler<?php echo the_title(); ?>" data-profile="<?php echo $loop->post->ID; ?>">
-			        <?php $featured_image = wp_get_attachment_image_src( get_post_thumbnail_id($loop->post->ID)); ?>
-			        <?php if($featured_image) { ?>
-			        <img src="<?php $featured_image[0]; ?>" data-id="<?php echo $loop->post->ID; ?>">
-			        <?php } ?>
-			        <p><?php the_title(); ?></p>
-			        <span class="price"><?php echo $product->get_price_html(); ?></span>
-			    </div>
-			<?php endwhile; ?> -->
 
 		</div>
 	</div>
