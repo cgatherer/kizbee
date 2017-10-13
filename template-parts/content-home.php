f6fa208da87d1c8d8ffa0148aa7eb8cb6a25bdcd
Christopher Gatherer <box@ChristophersMBP.home>
2017-10-12 23:07:46 -0400

    products image

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -12,22 +12,13 @@ global $product;
 ?>
 
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<!-- <div class="entry-header">
-		<?php the_title( '<h1>', '</h1>' ); ?>
-	</div> -->
-
-	<!-- <div class="entry-content">
-		<?php
-			the_content();
-		?>
-	</div> -->
 
 	<div class="container">
 		<div class="span12 margin-top">
 			<?php
 			  	$taxonomy     = 'product_cat';
 			  	$orderby      = 'name';  
-			  	$show_count   = 0;
+			  	$show_count   = 0ž
 			  	$pad_counts   = 0;
 			  	$hierarchical = 1;
 			  	$title        = '';  
@@ -73,7 +64,7 @@ global $product;
 						
 						<div class="span6 tiles" style="background: url('<?php the_field( 'new_product_image' ); ?>'); background-size: cover;">
 							<h2><?php the_title();?></h2>
-							<p class="price"><?php echo $product->get_price_html(); ?></p>
+
 						</div>
 					
 					<?php endwhile;
@@ -84,6 +75,21 @@ global $product;
 					     <?php _e( 'No Products' ); // (6) ?>
 					</p>
 		<?php endif; ?>
+
+		<?php
+			$args = array( 'post_type' => 'product', 'posts_per_page' => 80, 'product_cat' => 'profiler', 'orderby' => 'rand' );
+
+			$loop = new WP_Query( $args );
+			while ( $loop->have_posts() ) : $loop->the_post(); global $product; ?>
+			    <div class="dvThumb col-xs-4 col-sm-3 col-md-3 profiler-select profiler<?php echo the_title(); ?>" data-profile="<?php echo $loop->post->ID; ?>">
+			        <?php $featured_image = wp_get_attachment_image_src( get_post_thumbnail_id($loop->post->ID)); ?>
+			        <?php if($featured_image) { ?>
+			        <img src="<?php $featured_image[0]; ?>" data-id="<?php echo $loop->post->ID; ?>">
+			        <?php } ?>
+			        <p><?php the_title(); ?></p>
+			        <span class="price"><?php echo $product->get_price_html(); ?></span>
+			    </div>
+			<?php endwhile; ?>
 
 		</div>
 	</div>
