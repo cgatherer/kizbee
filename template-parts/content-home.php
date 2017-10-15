6bb5fc4201e31a3c66d42b934c62988dc7882803
Christopher Gatherer <box@ChristophersMBP.fios-router.home>
2017-10-15 13:04:54 -0400

    products image

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -62,7 +62,7 @@
 				    <div class="span6 tiles pack-item" style="background: url('<?php the_field( 'new_product_image' ); ?>'); background-size: cover;">
 						<h2><?php the_title();?></h2>
 						<div class="price"><?php echo $product->get_price_html(); ?></div>
-						<?php echo print_r($term); ?>
+						<?php echo $term; ?>
 					</div>
 			<?php endwhile; ?>
 		</div>
