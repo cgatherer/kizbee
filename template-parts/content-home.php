766f773acc4a638c715af33c93e2eed0e6d92cd6
Christopher Gatherer <box@ChristophersMBP.fios-router.home>
2017-10-15 14:35:12 -0400

    products image

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -8,8 +8,6 @@
  */
 ?>
 
-<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
 	<!-- <div id="home-product" class="container pack-grid">
 		<div class="span12 margin-top margin-bottom"> -->
 				
@@ -71,11 +69,45 @@
 		</div>
 	</div> -->
 
-	<div>
-		<?php the_content();?>
-	</div>
+<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</div><!-- .entry-header -->
 
-</div>
+	<div class="entry-content">
+		<?php
+			the_content();
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'kizbees_kitchen' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<?php if ( get_edit_post_link() ) : ?>
+		<footer class="entry-footer">
+			<?php
+				edit_post_link(
+					sprintf(
+						wp_kses(
+							/* translators: %s: Name of current post. Only visible to screen readers */
+							__( 'Edit <span class="screen-reader-text">%s</span>', 'kizbees_kitchen' ),
+							array(
+								'span' => array(
+									'class' => array(),
+								),
+							)
+						),
+						get_the_title()
+					),
+					'<span class="edit-link">',
+					'</span>'
+				);
+			?>
+		</footer><!-- .entry-footer -->
+	<?php endif; ?>
+</div><!-- #post-<?php the_ID(); ?> -->
 
 <script type="text/javascript">
   jQuery(document).ready(function($) {
