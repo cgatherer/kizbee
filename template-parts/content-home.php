1bb257945888219ebf63b338371f725e8cab9085
Christopher Gatherer <box@ChristophersMBP.fios-router.home>
2017-10-15 12:33:05 -0400

    products image

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -56,13 +56,12 @@
 				$loop = new WP_Query( $args );
 				while ( $loop->have_posts() ) : $loop->the_post(); global $product; ?>
 					<?php 
-						$terms = get_the_terms( $product->get_id(), 'product_cat' ); 
-						$termName = $terms->name; ?>
+						$terms = get_the_terms( $product->get_id(), 'product_cat' ); ?>
 
 				    <div class="span6 tiles pack-item" style="background: url('<?php the_field( 'new_product_image' ); ?>'); background-size: cover;">
 						<h2><?php the_title();?></h2>
 						<div class="price"><?php echo $product->get_price_html(); ?></div>
-						<p><?php echo $termName;?></p>
+						<p><?php single_term_title(); ?></p>
 					</div>
 			<?php endwhile; ?>
 		</div>
