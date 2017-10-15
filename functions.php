5955f2d5010bff15acb7cdbe9e902033af24b112
Christopher Gatherer <box@ChristophersMBP.fios-router.home>
2017-10-15 15:12:50 -0400

    products image

--- functions.php
+++ functions.php
@@ -179,3 +179,17 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 if ( class_exists( 'WooCommerce' ) ) {
 	require get_template_directory() . '/inc/woocommerce.php';
 }
+
+if (function_exists('register_sidebar')) {
+
+	register_sidebar(array(
+		'name' => 'Widgetized Area',
+		'id'   => 'widgetized-area',
+		'description'   => 'This is a widgetized area.',
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h4>',
+		'after_title'   => '</h4>'
+	));
+
+}
