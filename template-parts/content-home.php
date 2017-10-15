67cfb44c118387ccd5197d98bea2a36bc48632d5
Christopher Gatherer <box@ChristophersMBP.fios-router.home>
2017-10-15 12:16:32 -0400

    products image

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -55,7 +55,7 @@
 
 				$loop = new WP_Query( $args );
 				while ( $loop->have_posts() ) : $loop->the_post(); global $product; ?>
-				    <div class="span6 tiles pack-item" style="background: url('<?php the_field( 'new_product_image' ); ?>'); background-size: cover;">
+				    <div class="span6 tiles pack-item <?php echo $product->get_cat_name();?>" style="background: url('<?php the_field( 'new_product_image' ); ?>'); background-size: cover;">
 						<h2><?php the_title();?></h2>
 						<div class="price"><?php echo $product->get_price_html(); ?></div>
 					</div>
