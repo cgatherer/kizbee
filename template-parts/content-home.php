59ab34081507b74b46895abbd6a1f8dc40ab9efa
Christopher Gatherer <box@ChristophersMBP.fios-router.home>
2017-10-15 14:38:18 -0400

    products image

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -8,9 +8,26 @@
  */
 ?>
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</div><!-- .entry-header -->
+	<div class="span12 group margin-bottom">
+		<?php
+			$args = array('post_type' => 'product', 'posts_per_page' => 8);
+
+				$loop = new WP_Query( $args );
+				while ( $loop->have_posts() ) : $loop->the_post(); global $product; ?>
+					<?php 
+						$term = $product->get_categories(); ?>
+
+				    <div class="span6 tiles pack-item" style="background: url('<?php the_field( 'new_product_image' ); ?>'); background-size: cover;">
+						<h2><?php the_title();?></h2>
+						<div class="price"><?php echo $product->get_price_html(); ?></div>
+						<p><?php echo $term; ?></p>
+					</div>
+			<?php endwhile; ?>
+
+		<div class="span12 margin-bottom" style="text-align: center;">
+			<h2>Details About Shipping &amp; Availability</h2>
+		</div>
+	</div>
 
 	<div class="entry-content">
 		<?php
