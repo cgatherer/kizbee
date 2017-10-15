0993cd33285f8864aa96ce1350b746b252b1519f
Christopher Gatherer <box@ChristophersMBP.fios-router.home>
2017-10-15 11:58:14 -0400

    products image

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -54,7 +54,7 @@
 				while ( $loop->have_posts() ) : $loop->the_post(); global $product; ?>
 				    <div class="span6 tiles pack-item" style="background: url('<?php the_field( 'new_product_image' ); ?>'); background-size: cover;">
 						<h2><?php the_title();?></h2>
-						<span class="price"><?php echo $product->get_price_html(); ?></span>
+						<div class="price"><?php echo $product->get_price_html(); ?></div>
 					</div>
 			<?php endwhile; ?>
 		</div>
