f400385b272e190e79d6ae59b9735bd74c1fac9e
Christopher Gatherer <box@christophersmbp.hibbertnet.com>
2017-10-18 14:13:17 -0400

    New widgit functions

--- inc/widget-functions.php
+++ inc/widget-functions.php
@@ -11,9 +11,24 @@
  *  Widgets | Add
  */
 
-if( ! function_exists( 'kizbees_kitchen_register_widget' )){
-     function kizbees_kitchen_register_widget() {
-         register_widget('kizbees_kitchen_product_Widget'); // Products
-     }
+// Register and load the widget
+function wpb_load_widget() {
+    register_widget( 'wpb_widget' );
 }
-add_action('widgets_init','kizbees_kitchen_register_widget');
+add_action( 'widgets_init', 'wpb_load_widget' );
+ 
+// Creating the widget 
+class wpb_widget extends WP_Widget {
+ 
+function __construct() {
+parent::__construct(
+ 
+// Base ID of your widget
+'wpb_widget', 
+ 
+// Widget name will appear in UI
+__('WPBeginner Widget', 'wpb_widget_domain'), 
+ 
+// Widget description
+array( 'description' => __( 'Sample widget based on WPBeginner Tutorial', 'wpb_widget_domain' ), ));
+}
--- inc/widget-products.php
+++ inc/widget-products.php
@@ -7,96 +7,43 @@
  * @package Kizbees_Kitchen
  */
 
-class kizbees_kitchen_register_widget extends WP_Widget {
-	
-	/* ---------------------------------------------------------------------------
-	 * Constructor
-	 * --------------------------------------------------------------------------- */
-	function __construct(){
-		
-		$widget_ops = array( 'classname' => 'widget_kizbees_kitchen_podcasts', 'description' => __( 'The most recent products on your site.' ) );
-		
-		parent::__construct( 'widget_kizbees_kitchen_events', __( 'Product Widget' ), $widget_ops );
-		
-		$this->alt_option_name = 'widget_kizbees_kitchen_podcasts';
-	}
-	
-	
-	/* ---------------------------------------------------------------------------
-	 * Outputs the HTML for this widget.
-	 * --------------------------------------------------------------------------- */
-	function widget( $args ) {
-		
-		$args = array(
-			'posts_per_page' => 1,
-			'post_type'      => 'post'
-		);
-		
-		$r = new WP_Query($args);
-		
-		if ($r->have_posts()){     
-			     
-			echo '<div class="span12">';
-					
-					while ( $r->have_posts() ){
-						$r->the_post();
-						echo the_content();
-					}
-					wp_reset_postdata();
-
-			echo '</div>'."\n";
-		}
-	}
-
-
-	/* ---------------------------------------------------------------------------
-	 * Deals with the settings when they are saved by the admin.
-	 * --------------------------------------------------------------------------- */
-	function update( $new_instance, $old_instance ) {
-		$instance = $old_instance;
-		
-		$instance['title'] 		= strip_tags( $new_instance['title'] );
-		$instance['count'] 		= (int) $new_instance['count'];
-		$instance['category'] 	= strip_tags( $new_instance['category'] );
-		
-		return $instance;
-	}
-
-	
-	/* ---------------------------------------------------------------------------
-	 * Displays the form for this widget on the Widgets page of the WP Admin area.
-	 * --------------------------------------------------------------------------- */
-	function form( $instance ) {
-		
-		$title		= isset( $instance['title']) ? esc_attr( $instance['title'] ) : '';
-		$count		= isset( $instance['count'] ) ? absint( $instance['count'] ) : 5;
-		$category	= isset( $instance['category']) ? esc_attr( $instance['category'] ) : '';
-
-		?>
-			<p>
-				<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php _e( 'Title:', 'mfn-opts' ); ?></label>
-				<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
-			</p>
-			
-			<p>
-				<label for="<?php echo esc_attr( $this->get_field_id( 'category' ) ); ?>"><?php _e( 'Category:', 'mfn-opts' ); ?></label>
-				<select id="<?php echo esc_attr( $this->get_field_id( 'category' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'category' ) ); ?>" class="widefat" >
-					<?php 
-						$categories = mfn_get_categories( 'category' );
-						foreach( $categories as $k=>$v ){
-							$selected = ( $category == $k ) ? 'selected="selected"' : false;
-							echo '<option value="'. $k .'" '. $selected .'>'. $v .'</option>';
-						}
-					?>
-				</select>
-			</p>
-			
-			<p>
-				<label for="<?php echo esc_attr( $this->get_field_id( 'count' ) ); ?>"><?php _e( 'Number of posts:', 'mfn-opts' ); ?></label>
-				<input id="<?php echo esc_attr( $this->get_field_id( 'count' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'count' ) ); ?>" type="text" value="<?php echo esc_attr( $count ); ?>" size="3"/>
-			</p>
-			
-		<?php
-	}
+// Creating widget front-end
+ 
+public function widget( $args, $instance ) {
+$title = apply_filters( 'widget_title', $instance['title'] );
+ 
+// before and after widget arguments are defined by themes
+echo $args['before_widget'];
+if ( ! empty( $title ) )
+echo $args['before_title'] . $title . $args['after_title'];
+ 
+// This is where you run the code and display the output
+echo __( 'Hello, World!', 'wpb_widget_domain' );
+echo $args['after_widget'];
 }
+         
+// Widget Backend 
+public function form( $instance ) {
+if ( isset( $instance[ 'title' ] ) ) {
+$title = $instance[ 'title' ];
+}
+else {
+$title = __( 'New title', 'wpb_widget_domain' );
+}
+// Widget admin form
+?>
+<p>
+<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+</p>
+<?php 
+}
+     
+// Updating widget replacing old instances with new
+public function update( $new_instance, $old_instance ) {
+$instance = array();
+$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+return $instance;
+}
+} // Class wpb_widget ends here
 ?>
