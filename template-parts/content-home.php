872e71516d1c4c2707c6620098bff3f2dd1e245f
Christopher Gatherer <box@Christophers-MacBook-Pro.local>
2017-10-19 14:55:52 -0400

    Output widget

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -10,31 +10,7 @@
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<div class="entry-content">
-		<?php
-			the_content();
-		?>
-	</div><!-- .entry-content -->
+		<?php the_content();?>
+	</div>
 
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-				edit_post_link(
-					sprintf(
-						wp_kses(
-							/* translators: %s: Name of current post. Only visible to screen readers */
-							__( 'Edit <span class="screen-reader-text">%s</span>', 'kizbees_kitchen' ),
-							array(
-								'span' => array(
-									'class' => array(),
-								),
-							)
-						),
-						get_the_title()
-					),
-					'<span class="edit-link">',
-					'</span>'
-				);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
-</div><!-- #post-<?php the_ID(); ?> -->
+</div>
