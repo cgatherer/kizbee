9acb0b2085009fc11f2f1dc028eb09d37e3d5787
Christopher Gatherer <box@ChristophersMBP.home>
2017-10-19 21:33:08 -0400

    product loop

--- template-parts/content-product-loop.php
+++ template-parts/content-product-loop.php
@@ -48,17 +48,19 @@
 
 	<div class="span12 group margin-bottom">
 		<?php
+
 			$args = array('post_type' => 'product', 'posts_per_page' => 8);
 
 				$loop = new WP_Query( $args );
 					while ( $loop->have_posts() ) : $loop->the_post(); global $product; ?>
 						<?php 
-							$term = $product->get_categories(); ?>
+							$term = $product->get_categories(); 
+							$taxonomy = 'product_cat';?>
 
 				    		<div class="span6 tiles pack-item" style="background: url('<?php the_field( 'new_product_image' ); ?>'); background-size: cover;">
 								<h2><?php the_title();?></h2>
 								<div class="price"><?php echo $product->get_price_html(); ?></div>
-								<p><?php echo $term; ?></p>
+								<p><?php echo $term; ?> <?php echo $taxonomy;?></p>
 							</div>
 		<?php endwhile; ?>
 	</div>
