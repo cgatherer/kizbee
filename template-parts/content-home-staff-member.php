c67dfd6fc619f3f0931cd171a9ccb0d741621079
Christopher Gatherer <box@ChristophersMBP.fios-router.home>
2017-11-05 12:12:57 -0500

    Staff member

--- template-parts/content-home-staff-member.php
+++ template-parts/content-home-staff-member.php
@@ -15,17 +15,8 @@
 			$args = array('post_type' => 'staff-member', 'posts_per_page' => 2);
 
 				$loop = new WP_Query( $args );
-					while ( $loop->have_posts() ) : $loop->the_post(); 
-
-						?>
+					while ( $loop->have_posts() ) : $loop->the_post(); ?>
 						<h2><?php the_title();?></h2>
-						<div span="span4">
-							<?php echo get_the_post_thumbnail( $loop->ID, 'thumbnail' ); ?>
-						</div>
-						<div span="span8">
-							<p><?php the_excerpt();?></p>
-							<a href="<?php echo get_permalink(); ?>"> Read More...</a>
-						</div>
 		<?php endwhile; ?>
 	</div>
 </div>
