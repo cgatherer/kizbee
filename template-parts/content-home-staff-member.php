19940eb21a977881e422314e34eefe74e3d5c021
Christopher Gatherer <box@ChristophersMBP.home>
2017-11-06 18:07:27 -0500

    Home page

--- template-parts/content-home-staff-member.php
+++ template-parts/content-home-staff-member.php
@@ -11,18 +11,11 @@
 <div class="container pack-grid">
 	<div class="span12 group margin-bottom">
 		<?php
-			$args = array('post_type' => 'product', 'posts_per_page' => 8);
+			$args = array('post_type' => 'Staff Member', 'posts_per_page' => 8);
 
 				$loop = new WP_Query( $args );
-					while ( $loop->have_posts() ) : $loop->the_post(); global $product; ?>
-						<?php 
-							$term = $product->get_categories();?>
-
-				    		<div class="span6 tiles pack-item" style="background: url('<?php the_field( 'new_product_image' ); ?>'); background-size: cover;">
-								<h2><?php the_title();?></h2>
-								<div class="price"><?php echo $product->get_price_html(); ?></div>
-								<p><?php echo $term; ?></p>
-							</div>
+					while ( $loop->have_posts() ) : $loop->the_post(); ?>
+						<h2><?php the_title();?></h2>
 		<?php endwhile; ?>
 	</div>
 </div>
