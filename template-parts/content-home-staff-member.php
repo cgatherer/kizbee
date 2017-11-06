04629baf85e23255f25245aac4ca36905d6de631
Christopher Gatherer <box@ChristophersMBP.home>
2017-11-06 17:47:05 -0500

    Home page

--- template-parts/content-home-staff-member.php
+++ template-parts/content-home-staff-member.php
@@ -11,9 +11,7 @@
 <div class="container pack-grid">
 	<div class="span12 group margin-bottom">
 		<?php
-
 			$args = array('post_type' => 'staff-member', 'posts_per_page' => 2);
-
 				$loop = new WP_Query( $args );
 					while ( $loop->have_posts() ) : $loop->the_post(); ?>
 						<h2><?php the_title();?></h2>
